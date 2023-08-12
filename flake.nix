30a34646a765e48d08daf628d6b0448062902fd5
Gabor Pihaj <gabor.pihaj@gmail.com>
2023-08-12 11:57:14 +0100

    build: upgrade nix-rust-utils and nixpkgs

--- flake.nix
+++ flake.nix
@@ -1,5 +1,11 @@
 {
-  inputs.nix-rust-utils.url = "git+https://git.vdx.hu/voidcontext/nix-rust-utils.git";
+  inputs.nixpkgs.url = "nixpkgs/release-23.05";
+  inputs.nixpkgs-unstable.url = "nixpkgs/nixpkgs-unstable";
+
+  inputs.nix-rust-utils.url = "git+https://git.vdx.hu/voidcontext/nix-rust-utils.git?ref=refs/tags/v0.7.0";
+  inputs.nix-rust-utils.inputs.nixpkgs.follows = "nixpkgs";
+  inputs.nix-rust-utils.inputs.nixpkgs-unstable.follows = "nixpkgs-unstable";
+
   outputs = {nix-rust-utils, ...}:
     nix-rust-utils.lib.mkOutputs ({...}: {crate.src = ./.;});
 }
