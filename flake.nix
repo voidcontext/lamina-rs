bd7b945dcc3f382dee8e9ced6a68a4ee941ef550
Gabor Pihaj <gabor.pihaj@gmail.com>
2023-09-26 23:34:19 +0000

    build: upgrade nix rust utils to v0.8.1 (#7)
    Reviewed-on: https://git.vdx.hu/voidcontext/lamina-rs/pulls/7
    Co-authored-by: Gabor Pihaj <gabor.pihaj@gmail.com>
    Co-committed-by: Gabor Pihaj <gabor.pihaj@gmail.com>

--- flake.nix
+++ flake.nix
@@ -1,11 +1,40 @@
 {
   inputs.nixpkgs.url = "nixpkgs/release-23.05";
   inputs.nixpkgs-unstable.url = "nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  inputs.nix-rust-utils.url = "git+https://git.vdx.hu/voidcontext/nix-rust-utils.git?ref=refs/tags/v0.7.0";
+  inputs.nix-rust-utils.url = "git+https://git.vdx.hu/voidcontext/nix-rust-utils.git?ref=refs/tags/v0.8.1";
   inputs.nix-rust-utils.inputs.nixpkgs.follows = "nixpkgs";
   inputs.nix-rust-utils.inputs.nixpkgs-unstable.follows = "nixpkgs-unstable";
 
-  outputs = {nix-rust-utils, ...}:
-    nix-rust-utils.lib.mkOutputs ({...}: {crate.src = ./.;});
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    nix-rust-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+      nru = nix-rust-utils.mkLib {inherit pkgs;};
+      commonArgs = {
+        src = ./.;
+        buildInputs = pkgs.lib.optionals pkgs.stdenv.isDarwin [
+          pkgs.libiconv
+        ];
+      };
+
+      crate = nru.mkCrate (commonArgs // {
+        doCheck = false;
+      });
+      checks = nru.mkChecks (commonArgs // {
+        inherit crate;
+        nextest = true;
+      });
+    in {
+      inherit checks;
+
+      packages.default = crate;
+
+      devShells.default = nru.mkDevShell {inputsFrom = [crate]; inherit checks;};
+    });
 }
