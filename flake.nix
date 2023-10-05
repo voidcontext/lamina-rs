d89dda8b4b175f166e3e9cfc478348a561207c18
Gabor Pihaj <gabor.pihaj@gmail.com>
2023-10-05 21:03:09 +0000

    feat: add overlays to flake output (#13)
    Reviewed-on: https://git.vdx.hu/voidcontext/lamina-rs/pulls/13
    Co-authored-by: Gabor Pihaj <gabor.pihaj@gmail.com>
    Co-committed-by: Gabor Pihaj <gabor.pihaj@gmail.com>

--- flake.nix
+++ flake.nix
@@ -13,16 +13,19 @@
     nix-rust-utils,
     ...
   }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs {inherit system;};
+    let 
+    mkLamina = pkgs: 
+    let 
       nru = nix-rust-utils.mkLib {inherit pkgs;};
+
       commonArgs = {
         src = ./.;
         buildInputs = pkgs.lib.optionals pkgs.stdenv.isDarwin [
           pkgs.libiconv
         ];
       };
-
+    in
+    rec {
       crate = nru.mkCrate (commonArgs
         // {
           doCheck = false;
@@ -41,14 +44,30 @@
           inherit crate;
           nextest = true;
         });
+    } ;
+    outputs = 
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};    
+      nru = nix-rust-utils.mkLib {inherit pkgs;};
+      
+      lamina = mkLamina pkgs;
     in {
-      checks = builtins.removeAttrs checks ["cargo-nextest"];
+      checks = builtins.removeAttrs lamina.checks ["cargo-nextest"];
 
-      packages.default = crate;
+      packages.default = lamina.crate;
 
       devShells.default = nru.mkDevShell {
-        inputsFrom = [crate];
-        inherit checks;
+        inputsFrom = [lamina.crate];
+        inherit (lamina) checks;
       };
     });
+  in outputs // {
+    overlays.default = final: prev: {
+      lamina = outputs.packages.${final.system}.default;
+    };
+    
+    overlays.withHostPkgs = final: prev: {
+      lamina = (mkLamina final).crate;
+    };
+  };
 }
